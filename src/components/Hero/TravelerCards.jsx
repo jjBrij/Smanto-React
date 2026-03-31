db1dfae47d0b01a2e3c57f341314ab501e15088f
B MOHAN PRASAD <brijmohan2204@gmail.com>
2026-04-01 01:15:51 +0530

    fix: add missing TravelerCards.css

--- src/components/Hero/TravelerCards.jsx
+++ src/components/Hero/TravelerCards.jsx
@@ -3,7 +3,6 @@ import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { getTopRoutes } from '../../services/api'
 import { getToken, getUser } from '../../utils/helpers'
-import './TravelerCards.css'
 
 const FALLBACK = []
 
